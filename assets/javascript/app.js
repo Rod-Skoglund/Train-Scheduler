f10fbfa4f3f9b9d6a0fdb0dc5b928e5920b68c47
RLS <rod.skoglund@gmail.com>
2018-06-26 20:23:47 -0500

    Fixed bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,8 +40,8 @@ else if (isNaN(frequency)) {
     alert("Please enter a number for the frequency.");
 }
 //test for valid time input
-else if (!moment(firstTrainTime, "hh:mm").isValid()) {
-    alert("Please enter a valid time for the first train (HH:MM).");
+else if (!moment(firstTrainTime, "HH:mm").isValid()) {
+    alert("Please enter a valid time for the first train (HH:mm).");
 }
 else {
     // All user inputs are valid
@@ -60,18 +60,18 @@ else {
 database.ref("schedule/").on("child_added", function(childSnapshot) {
 
     // Capture data from DB
-    var thisTrainName = childSnapshot.val().trainName;
-    var thisDestination = childSnapshot.val().destination;
+    // var thisTrainName = childSnapshot.val().trainName;
+    // var thisDestination = childSnapshot.val().destination;
     var thisFirstTrainTime = childSnapshot.val().firstTrainTime;
     var thisFrequency = childSnapshot.val().frequency;
 
     //Check to see if the first train time is in the future
-    var isFirstTrainInFuture = (thisFirstTrainTime > moment().format("hh:mm"));
+    var isFirstTrainInFuture = (thisFirstTrainTime > moment().format("HH:mm"));
 
     // Convert the Frequency from the DB to an Integer
     thisFreq = parseInt(thisFrequency);
 
-    currentTime = moment().format("hh:mm");
+    currentTime = moment().format("HH:mm");
 
     // define variables for calculated fields
     var minAway;
@@ -80,21 +80,23 @@ database.ref("schedule/").on("child_added", function(childSnapshot) {
     // If the firt train is in the future, set the next train to the first train time (has not happened yet)
     // and calculate the time to next traing as the difference between the current time and the first train time
     if (isFirstTrainInFuture) {
-        minAway = (moment(childSnapshot.val().firstTrainTime, "hh:mm").diff(moment(), "minutes") + 1);
-        nextArrival = moment(childSnapshot.val().firstTrainTime, "hh:mm");
+        minAway = (moment(childSnapshot.val().firstTrainTime, "HH:mm").diff(moment(), "minutes") + 1);
+        nextArrival = moment(childSnapshot.val().firstTrainTime, "HH:mm");
+        console.log("first train is in the future");
     }
     // The first train time is in the past. Calculate the minutes to the next train and the next arrival 
     // time for this train
     else {
-        minAway = thisFreq - (moment().diff(moment(childSnapshot.val().firstTrainTime, "hh:mm"), "minutes")) % thisFreq;
+        minAway = thisFreq - (moment().diff(moment(childSnapshot.val().firstTrainTime, "HH:mm"), "minutes")) % thisFreq;
         nextArrival = moment().add(minAway, "minutes");
+
     }
 
     // Add the train to the Train Schedule table.
     $(".train-list").append("<tr><td>" + childSnapshot.val().trainName + "</td>" + 
         "<td>" + childSnapshot.val().destination + "</td>" + 
         "<td>" + childSnapshot.val().frequency + "</td>" + 
-        "<td>" + nextArrival.format("hh:mm") + "</td>" + 
+        "<td>" + nextArrival.format("HH:mm") + "</td>" + 
         "<td>" + minAway + "</td></tr>");
         
     // Handle the errors
